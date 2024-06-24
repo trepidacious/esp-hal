6ffb7d77e98b2205a5fd399e36799c99328918b6
trepidacious <trepidacious@gmail.com>
2024-06-24 19:48:47 +0100

    Remove projects that are now referenced by git

--- esp-hal/ld/esp32s2/linkall.x
+++
@@ -1,5 +0,0 @@
-INCLUDE "memory.x"
-INCLUDE "alias.x"
-INCLUDE "esp32s2.x"
-INCLUDE "hal-defaults.x"
-INCLUDE "rom-functions.x"
--- esp-ieee802154/build.rs
+++
@@ -1,6 +0,0 @@
-use std::{env, path::PathBuf};
-
-fn main() {
-    let out = PathBuf::from(env::var_os("OUT_DIR").unwrap());
-    println!("cargo:rustc-link-search={}", out.display());
-}
